07893fb83d3c295c64eefdda9d88ce9bcf7342ac
Annie0329 <0329annie@gmail.com>
2022-01-14 22:11:46 +0800

    0114

--- DiaTest/export/html5/haxe/ApplicationMain.hx
+++ DiaTest/export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "2632";
+		app.meta["build"] = "2634";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "DiaTest";
 		app.meta["name"] = "DiaTest";
