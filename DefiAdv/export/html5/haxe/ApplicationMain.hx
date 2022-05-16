bb0cecf37a2a0ca4b71344ffc39679795497cac0
Annie0329 <0329annie@gmail.com>
2022-05-16 16:35:49 +0800

    0516

--- DefiAdv/export/html5/haxe/ApplicationMain.hx
+++ DefiAdv/export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "3887";
+		app.meta["build"] = "3890";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "DefiAdv";
 		app.meta["name"] = "DefiAdv";
